90f41e1c194a6835231c3789ecc46be2a77444a2
Bob Kwiencien <bkwiencien@gmail.com>
2017-07-28 20:13:28 -0400

    Jul28-20:23

--- hangman.js
+++ hangman.js
@@ -1,4 +1,13 @@
 var myWord="water";
+var array = ["w","a","t","e","r"];
+var done=false;
+var letterguesedraw = " ";
+var letterguessed = " ";
 function yes() {
+while (!done) {
+ letterguessedraw = prompt("guess a letter");
+ letterguessed = letterguessedraw.toLowerCase();
+ console.log(letterguessed);
+}
 }
 
