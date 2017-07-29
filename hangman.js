148b4f1d6e73e19dd960182a2e09d06604b0f1dc
Bob Kwiencien <bkwiencien@gmail.com>
2017-07-29 19:10:15 -0400

    Jul29-19:09

--- hangman.js
+++ hangman.js
@@ -3,6 +3,8 @@ var array = ["w","a","t","e","r"];
 var done=false;
 var letterguesedraw = " ";
 var letterguessed = " ";
+var sofar = "     ";
+var initialString = "__ __ __ __ __";
 function yes() {
 while (!done) {
  letterguessedraw = prompt("guess a letter");
