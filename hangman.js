d57bd08e19378cf8e54833c27438ddbede61dae7
Bob Kwiencien <bkwiencien@gmail.com>
2017-07-30 14:47:26 -0400

    Jul30-14:47:

--- hangman.js
+++ hangman.js
@@ -3,15 +3,22 @@ array = ["w","a","t","e","r"];
 var newyWord = "";
 var word = "water";
 var done=false;
-var letterguesedraw = " ";
-var letterguessed = " ";
+var letterGuesedRaw = " ";
+var letterGuessed = " ";
 var sofar = "     ";
 var initialString = "__ __ __ __ __";
-function yes() {
-while (!done) {
- letterguessedraw = prompt("guess a letter");
- letterguessed = letterguessedraw.toLowerCase();
- console.log(letterguessed);
+var firstLetter = "  __ __ __ __"
+var secondLetter = " __  __ __ __";
+function getData() {
+ while (!done) {
+  var input=document.getElementById("userinput")
+   letterGuessedRaw = input.value;
+   len = letterGuessedRaw.length;
+   if (len > 1) {
+     letterGuessedRaw = letterGuessedRaw.charAt(0);
+     letterGuessed = lertterGuessedRaw.toLowerCase();
+   }
+  console.log("you guessed " + letterGuessed);
 }
 }
 
