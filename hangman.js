4858c13a240f3725b2d95c9cea0bb94423735fa5
Bob Kwiencien <bkwiencien@gmail.com>
2017-07-31 12:51:49 -0400

    Jul31-12:51

--- hangman.js
+++ hangman.js
@@ -8,9 +8,11 @@ var letterGuessedRaw = " ";
 var alreadyGuessed = " ";
 var sofar = "     ";
 var initialString = "__ __ __ __ __";
+var currentLine   = "__ __ __ __ __";
 var firstLetter = "  __ __ __ __"
 var secondLetter = " __  __ __ __";
 var numberOfGuesses = 0;
+var searchStatus = 0;
 function getData() {
  if (!done) {
   var input=document.getElementById("userinput")
@@ -25,6 +27,17 @@ function getData() {
      letterGuessed = letterGuessedRaw.toLowerCase();
      alreadyGuessed = alreadyGuessed + letterGuessed;
      numberOfGuesses++;
+     searchStatus = searchFor(letterGuessed);
+     if (searchStatus == -1) {
+        var element1 = document.getElementById("resulto");
+        element1.innerHTML = "result: " + "there is no " + letterGuessed;
+     } 
+     if (searchStatus > -1) {
+        var element2 = document.getElementById("resulto");
+        console.log("i am in the right place");
+        console.log("element2 = " + element2);
+        element2.innerHTML = "success";
+     }
      console.log("alreadyGuessed = " + alreadyGuessed);
      var element  = document.getElementById("guessesline");
      element.innerHTML = "Guesses so far " + alreadyGuessed;
@@ -32,5 +45,14 @@ function getData() {
        done = true;
      }
 }
+}
+function searchFor(thisLetter) {
+ var indexo = "water".search(thisLetter);
+ console.log("indexo = " + indexo);
+ var notFound = -1;
+ if (indexo >= 0) {
+   return(indexo);
+ }
+ return (notFound); 
 }
 
